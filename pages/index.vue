e6e06e846c5e28144c3d80727118e84a0412bed3
Fabio Vedovelli <vedovelli@gmail.com>
2020-09-19 15:30:25 +0200

    Aula 005 - fonte de dados

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
       <div
         class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6"
       >
-        <product-card v-for="(item, index) in 12" :key="index" />
+        <product-card v-for="product in products" :key="product.id" />
       </div>
     </div>
   </main>
@@ -14,7 +14,13 @@
 
 <script>
 import ProductCard from '@/components/ProductCard'
+import products from '@/mocks/products.json'
 export default {
   components: { ProductCard },
+  computed: {
+    products() {
+      return products
+    },
+  },
 }
 </script>
