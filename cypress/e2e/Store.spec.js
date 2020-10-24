3e14bfd16f60328394583d8fccb089c3a74d3ac4
Fabio Vedovelli <vedovelli@gmail.com>
2020-10-24 12:50:23 +0200

    Aula 052 - Cypress Command addToCart

--- cypress/e2e/Store.spec.js
+++ cypress/e2e/Store.spec.js
@@ -23,8 +23,10 @@ context('Store', () => {
   });
 
   context.only('Store > Shopping Cart', () => {
+    const quantity = 10;
+
     beforeEach(() => {
-      server.createList('product', 10);
+      server.createList('product', quantity);
       cy.visit('/');
     });
 
@@ -50,11 +52,22 @@ context('Store', () => {
       gid('cart-item').should('have.length', 1);
     });
 
-    it.only('should add 3 products to the cart', () => {
-      gid('product-card').eq(1).find('button').click();
-      gid('product-card').eq(3).find('button').click({ force: true });
-      gid('product-card').eq(5).find('button').click({ force: true });
+    it('should add 3 products to the cart', () => {
+      cy.addToCart([1, 3, 5]);
+
       gid('cart-item').should('have.length', 3);
+    });
+
+    it('should add 1 product to the cart', () => {
+      cy.addToCart(6);
+
+      gid('cart-item').should('have.length', 1);
+    });
+
+    it('should add 1 product to the cart', () => {
+      cy.addToCart('all');
+
+      gid('cart-item').should('have.length', quantity);
     });
   });
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,42 @@
 Cypress.Commands.add('getByTestId', (selector) => {
   return cy.get(`[data-testid="${selector}"]`);
 });
+
+Cypress.Commands.add('addToCart', (mode) => {
+  cy.getByTestId('product-card').as('productCards');
+
+  const click = (index) =>
+    cy.get('@productCards').eq(index).find('button').click({ force: true });
+
+  const addByIndex = () => {
+    click(mode);
+  };
+
+  const addByIndexes = () => {
+    for (const index of mode) {
+      click(index);
+    }
+  };
+
+  const addAll = () => {
+    cy.get('@productCards').then(($elements) => {
+      let i = 0;
+      while (i < $elements.length) {
+        click(i);
+        i++;
+      }
+    });
+  };
+
+  if (Array.isArray(mode)) {
+    addByIndexes();
+  } else if (typeof mode === 'number') {
+    addByIndex();
+  } else if (typeof mode === 'string' && mode === 'all') {
+    addAll();
+  } else {
+    throw new Error(
+      'Please provide a valid input for cy.addToCart()\r\nPossible values are Array, number or "all"'
+    );
+  }
+});
