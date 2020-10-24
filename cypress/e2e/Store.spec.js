4e7e9efeb48615274f42fbd9b1d5b2dbb9e8acfb
Fabio Vedovelli <vedovelli@gmail.com>
2020-10-24 09:38:36 +0200

    Aula 044 - Configuracoes e primeiro teste

---
+++ cypress/e2e/Store.spec.js
@@ -0,0 +1,8 @@
+context('Store', () => {
+  it('should display the store', () => {
+    cy.visit('http://localhost:3000');
+
+    cy.get('body').contains('Brand');
+    cy.get('body').contains('Wrist Watch');
+  });
+});
